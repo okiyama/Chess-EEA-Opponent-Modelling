c5495577c1ef1e3c18aecf068ffc86e25a41d7d1
Julian Jocque <lamabuny@gmail.com>
2017-07-10 12:22:51 -0400

    Open Stockfish successfully

--- TestGround.py
+++ TestGround.py
@@ -1,6 +1,6 @@
 from CommandLineMoveGetter import CommandLineMoveGetter
 from RandomTestGetter import RandomTestGetter
-import chess
+import chess, time, chess.uci
 
 class TestGround:
     """ Just for testing things out, things in here will change rapidly """
@@ -22,8 +22,16 @@ class TestGround:
             randomTest.actualMove = move
             randomTest = testGetter.getNextTest(None, None)
 
+    def openUCIengine(self, location):
+        engine = chess.uci.popen_engine(location)
+        engine.uci()
+        engine.uciok.wait(10)
+        print(engine.name)
+        print(engine.author)
+        print(engine.options)
 
 if __name__ == "__main__":
     testGround = TestGround()
     # testGround.playNormalChessGame()
-    testGround.solveRandomTests()
+    # testGround.solveRandomTests()
+    testGround.openUCIengine("./stockfish-8-win/Windows/stockfish_8_x64.exe")
